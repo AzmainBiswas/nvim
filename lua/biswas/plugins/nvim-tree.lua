b406de0899b301f5d0019c0a7a4d5a6f9e06c7f5
Azmain Biswas <azmainbiswas007@gmail.com>
2024-02-07 01:46:30 +0530

    off nvim tree after opening a file

--- lua/biswas/plugins/nvim-tree.lua
+++ lua/biswas/plugins/nvim-tree.lua
@@ -7,6 +7,11 @@ return {
 	},
 	config = function()
 		require("nvim-tree").setup({
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
 			sort = {
 				sorter = "case_sensitive",
 			},
