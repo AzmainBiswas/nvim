f586dec6b6756c5c7ea200c2b8601e18045baa20
Azmain Biswas <azmainbiswas007@gmail.com>
2024-03-13 03:25:00 +0530

    Wed Mar 13 03:25:00 AM IST 2024

--- lua/biswas/plugins/nvim-cmp.lua
+++ lua/biswas/plugins/nvim-cmp.lua
@@ -35,7 +35,7 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
 				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-				["<TAB>"] = cmp.mapping.confirm({ select = true }),
+				-- ["<TAB>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
