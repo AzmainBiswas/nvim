3386c9f28b82a4051a8d1544a383a3b21dd97f4a
Azmain Biswas <azmainbiswas007@gmail.com>
2024-04-16 00:00:11 +0530

    Tue Apr 16 12:00:11 AM IST 2024

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,26 @@
 return {
-    { "nvim-lua/plenary.nvim" },
+	{ "nvim-lua/plenary.nvim" },
 
-    -- LSP progress messages.
-    {
-        "j-hui/fidget.nvim",
-        config = function()
-            require("fidget").setup({})
-        end,
-    },
-    {
-        "numToStr/Comment.nvim",
-        lazy = false,
-        config = function()
-            require("Comment").setup()
-        end,
-    },
+	-- LSP progress messages.
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+
+	-- Highlight todo, notes, etc in comments
+	{
+		"folke/todo-comments.nvim",
+		event = "VimEnter",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = { signs = false },
+	},
 }
--- lua/pre/options.lua
+++ lua/pre/options.lua
@@ -36,8 +36,8 @@ vim.opt.isfname:append("@-@")
 
 -- netrw
 -- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
