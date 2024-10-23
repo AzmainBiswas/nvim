7a1f312824884f4c207ac51cdcf90484c9c7b94a
Azmain Biswas <82164368+AzmainBiswas@users.noreply.github.com>
2024-10-24 00:28:09 +0530

    fix: which key nammings

--- after/ftplugin/c.vim
+++ after/ftplugin/c.vim
@@ -1,4 +1,4 @@
 setlocal shiftwidth=2
 setlocal commentstring=//%s
-
+setlocal makeprg=make
 " setlocal formatoptions-=o
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,30 +37,37 @@ return {
             local lsp_zero = require("lsp-zero")
             -- config for lsp-zero
             lsp_zero.on_attach(function(client, bufnr)
-                local opts = { buffer = bufnr, remap = false }
 
                 vim.keymap.set("n", "gd", function()
                     vim.lsp.buf.definition()
-                end, opts)
+                end, { desc = "go to difinition", buffer = bufnr, remap = false })
+
                 vim.keymap.set("n", "K", function()
                     vim.lsp.buf.hover()
-                end, opts)
+                end, { desc = "Peck difinition", buffer = bufnr, remap = false })
+
                 vim.keymap.set("n", "<leader>vws", function()
                     vim.lsp.buf.workspace_symbol()
-                end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+                end, { desc = "workspace_symbol", buffer = bufnr, remap = false })
+
+                vim.keymap.set("n", "<leader>vd", function()
+                    vim.diagnostic.open_float()
+                end, { desc = "open_float", buffer = bufnr, remap = false })
+
                 vim.keymap.set("n", "[d", function()
                     vim.diagnostic.goto_next()
-                end, opts)
+                end, { desc = "goto next", buffer = bufnr, remap = false })
+
                 vim.keymap.set("n", "]d", function()
                     vim.diagnostic.goto_prev()
-                end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+                end, { desc = "goto previous", buffer = bufnr, remap = false })
+
+                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, { desc = "Code Action", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, { desc = "Find References", buffer = bufnr, remap = false })
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, { desc = "Rename", buffer = bufnr, remap = false })
                 vim.keymap.set("i", "<C-h>", function()
                     vim.lsp.buf.signature_help()
-                end, opts)
+                end, { desc = "signature_help", buffer = bufnr, remap = false })
             end)
 
             lsp_zero.setup()
