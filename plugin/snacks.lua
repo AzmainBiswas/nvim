b204d7e1a9a63ccf69395e01e100c9a96c7c44ef
Azmain Biswas <azmainbiswas007@gmail.com>
2026-05-21 15:15:30 +0530

    addes snacks.picker and remove all the other picker

--- plugin/snacks.lua
+++ plugin/snacks.lua
@@ -3,24 +3,64 @@ vim.pack.add({
 })
 
 local Snacks = require("snacks")
+
 Snacks.setup({
     -- bigfile = {
-        --     enabled = true,
-        --     size = 1.5 * 1024 * 1024, -- 1.5MB
-        -- },
-        blame_line = {
-            enabled = true,
-        },
-        gitbrowse = {
-            enabled = true,
-        }
-    })
+    --     enabled = true,
+    --     size = 1.5 * 1024 * 1024, -- 1.5MB
+    -- },
+    blame_line = {
+        enabled = true,
+    },
+    gitbrowse = {
+        enabled = true,
+    },
+    picker = {
+        enabled = true,
+        ui_select = true,
 
-vim.api.nvim_create_user_command("GitBlam", function() 
+        layout = {
+            cycle = true,
+            preset = "vscode",
+        },
+
+        layouts = {
+            vscode = {
+                hidden = { "preview" },
+                layout = {
+                    backdrop = false,
+                    row = 2,
+                    width = 0.8,
+                    min_width = 100,
+                    height = 0.8,
+                    border = "none",
+                    box = "vertical",
+                    { win = "input",   height = 1,          border = true, title = "{title} {live} {flags}", title_pos = "center" },
+                    { win = "list",    border = "hpad" },
+                    { win = "preview", height = 0.6, title = "{preview}", border = true },
+                },
+            }
+        },
+    },
+})
+
+vim.api.nvim_create_user_command("GitBlam", function()
     Snacks.git.blame_line()
 end, { desc = "Git blame for line" })
 
 
-vim.api.nvim_create_user_command("GitBrowser", function() 
-    Snacks.gitbrowse() 
+vim.api.nvim_create_user_command("GitBrowser", function()
+    Snacks.gitbrowse()
 end, { desc = "Git blame for line" })
+
+local picker = Snacks.picker;
+
+-- picker.setup({
+-- })
+
+vim.keymap.set("n", "<leader>ff", picker.files, { desc = "Find Files" })
+vim.keymap.set("n", "<leader>fg", picker.grep, { desc = "Grep word from opened file system" })
+vim.keymap.set("n", "<leader>fb", picker.buffers, { desc = "switch buffers" })
+vim.keymap.set("n", "<leader>fh", picker.help, { desc = "help" })
+vim.keymap.set("n", "<leader>fc", picker.colorschemes, { desc = "change colorscheme" })
+vim.keymap.set("n", "<leader>fs", picker.grep_buffers, { desc = "Find in Buffer" })
