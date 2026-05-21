75cf3c7bf184ebc8d9bbe86f07d0a3e43a17f49c
Azmain Biswas <azmainbiswas007@gmail.com>
2026-05-21 13:39:12 +0530

    Show which LSP client is using in status line
     with '#' will be ignored, and an empty message aborts the commit.

--- plugin/init.lua
+++ plugin/init.lua
@@ -6,7 +6,6 @@ vim.pack.add({
     "https://github.com/lewis6991/gitsigns.nvim",
 })
 
-require("mini.statusline").setup()
 require("mini.icons").setup()
 
 require("which-key").add({
@@ -16,3 +15,56 @@ require("which-key").add({
 })
 
 require("gitsigns").setup()
+-- require("mini.statusline").setup()
+
+local function get_active_lsps()
+  local buf_clients = vim.lsp.get_clients({ bufnr = 0 })
+  if next(buf_clients) == nil then
+    return ""
+  end
+
+  local client_names = {}
+  for _, client in pairs(buf_clients) do
+    table.insert(client_names, client.name)
+  end
+  
+  -- Returns something like "[lua_ls, vtsls]"
+  return "[" .. table.concat(client_names, ", ") .. "]"
+end
+
+local statusline = require("mini.statusline")
+
+statusline.setup({
+  content = {
+    -- Override the active statusline layout
+    active = function()
+      -- 1. Grab all the default mini.statusline sections
+      local mode, mode_hl = statusline.section_mode({ trunc_width = 120 })
+      local git           = statusline.section_git({ trunc_width = 40 })
+      local diff          = statusline.section_diff({ trunc_width = 75 })
+      local diagnostics   = statusline.section_diagnostics({ trunc_width = 75 })
+      local lsp           = statusline.section_lsp({ trunc_width = 75 })
+      local filename      = statusline.section_filename({ trunc_width = 140 })
+      local fileinfo      = statusline.section_fileinfo({ trunc_width = 120 })
+      local location      = statusline.section_location({ trunc_width = 75 })
+
+      local lsp_name = get_active_lsps();
+      -- local progress = vim.ui.progress_status and vim.ui.progress_status() or ""
+
+      -- 3. Combine them all using mini's layout builder
+      return statusline.combine_groups({
+        { hl = mode_hl,                  strings = { mode } },
+        { hl = "MiniStatuslineDevinfo",  strings = { git, diff, diagnostics } },
+        -- Use %= to push everything after this to the center/right of the statusline
+        "%=", 
+        -- Place the filename block in the middle
+        { hl = "MiniStatuslineFilename", strings = { filename } },
+        "%=",
+        -- Inject the LSP progress on the right side next to the connected LSP names
+        { hl = "MiniStatuslineDevinfo",  strings = { lsp_name } },
+        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+        { hl = mode_hl,                  strings = { location } },
+      })
+    end,
+  },
+})
