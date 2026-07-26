1bd0a0c1945e32340d5514efe36611c8a57217d2
Azmain Biswas <azmainbiswas007@gmail.com>
2026-07-27 00:04:17 +0530

    REMOVE: removed ui2 it is showing error

--- init.lua
+++ init.lua
@@ -15,12 +15,12 @@ vim.cmd.packadd("nvim.undotree")
 vim.cmd.packadd("nvim.difftool")
 vim.cmd.packadd("cfilter")
 
-require('vim._core.ui2').enable({
-    msg = {
-        target = 'cmd',
-        cmd = { height = 1 }
-    }
-})
+-- require('vim._core.ui2').enable({
+--     msg = {
+--         target = 'cmd',
+--         cmd = { height = 1 }
+--     }
+-- })
 
 -- insert current time.
 vim.api.nvim_create_user_command("Now", function()
