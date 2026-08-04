a4dfb663d9e23206d5fc15068d140760104ba66e
Azmain Biswas <azmainbiswas007@gmail.com>
2026-08-04 23:53:54 +0530

    Change color to gruvbox theme

--- plugin/color.lua
+++ plugin/color.lua
@@ -2,19 +2,19 @@ vim.pack.add({
     -- { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
     -- "https://github.com/folke/tokyonight.nvim",
     -- { src = "https://github.com/rose-pine/neovim",                 name = "rose-pine" },
-    -- "https://github.com/ellisonleao/gruvbox.nvim"
-    { src = "https://github.com/maxmx03/solarized.nvim" },
+    "https://github.com/ellisonleao/gruvbox.nvim"
+    -- { src = "https://github.com/maxmx03/solarized.nvim" },
 })
 
 vim.opt.background = "dark"
 
 -- vim.cmd.colorscheme("habamax")
 -- vim.cmd.colorscheme("catppuccin-mocha")
-vim.cmd.colorscheme("solarized")
+-- vim.cmd.colorscheme("solarized")
 -- vim.cmd.colorscheme("tokyonight-night")
 -- vim.cmd.colorscheme("slate")
 -- vim.cmd.colorscheme("retrobox")
--- vim.cmd.colorscheme("gruvbox")
+vim.cmd.colorscheme("gruvbox")
 
 -- Source - https://stackoverflow.com/a
 -- Posted by C. Khalifa
