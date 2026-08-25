ee2d3f5079489a2689a29a0b99f7f23bb5a56e48
Azmain <azmainbiswas007@gmail.com>
2026-08-26 01:37:39 +0530

    Change color to catppuccin

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,20 +1,21 @@
 vim.pack.add({
     { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-    "https://github.com/folke/tokyonight.nvim",
-    { src = "https://github.com/rose-pine/neovim",                 name = "rose-pine" },
-    "https://github.com/ellisonleao/gruvbox.nvim"
+    -- "https://github.com/folke/tokyonight.nvim",
+    -- { src = "https://github.com/rose-pine/neovim",                 name = "rose-pine" },
+    -- "https://github.com/ellisonleao/gruvbox.nvim"
     -- { src = "https://github.com/maxmx03/solarized.nvim" },
 })
 
 vim.opt.background = "dark"
 
 -- vim.cmd.colorscheme("habamax")
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("catppuccin")
 -- vim.cmd.colorscheme("solarized")
 -- vim.cmd.colorscheme("tokyonight-night")
 -- vim.cmd.colorscheme("slate")
 -- vim.cmd.colorscheme("retrobox")
 -- vim.cmd.colorscheme("gruvbox")
+-- vim.cmd.colorscheme("default")
 
 -- Source - https://stackoverflow.com/a
 -- Posted by C. Khalifa
